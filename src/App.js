f76ea917d0a965b3da8dde6822a0b245c354cbb6
Agnes <81079528+agneslau@users.noreply.github.com>
2022-12-02 00:15:03 +0100

    Feat/load screens (#2)
    * add components for crossroad and straightroad. Fail to reload screen
    
    * roadmap from json works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
-import Question from './components/Question';
+import Screen from './components/Screen';
+
+import { useState } from 'react';
+import data from './mock/dataMocked.json';
 //import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [screen, setScreen] = useState(
+    data.filter(screen => screen.id === 998)[0]
+  );
+
+  const setNewData = newSlide => {
+    const newScreen = data.filter(screen => screen.id == newSlide)[0];
+    setScreen(newScreen);
+  };
   return (
     <div className="App">
       <Header></Header>
 
       <main>
-        <Question />
+        <Screen screen={screen} setNextSlide={setNewData} />
       </main>
     </div>
   );
---
+++ src/components/LastScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LastScreen({ prop, setNextSlide }) {
+  return (
+    <div className="lastScreen">
+      <p>C'est fini !!!</p>
+    </div>
+  );
+}
+
+export default LastScreen;
