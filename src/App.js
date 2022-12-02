68dff53a6d5f89ad5f7500dbf3d95f46aa5e9a30
Wazaray <louis.mtdd5@gmail.com>
2022-12-02 07:03:34 +0100

    fix: refacto some dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
 import Screen from './components/Screen';
 import BadScreen from './components/BadScreen';
 
@@ -20,7 +19,6 @@ function App() {
   };
   return (
     <div>
-      <Header />
       <main>
         {badScreen ? (
           <BadScreen
